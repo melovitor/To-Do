85be6da8cfbfb71e445cf40e0c275d3afd006d7e
melovitor <vitormelobatista@hotmail.com>
2023-07-23 00:24:43 -0300

    WIP - Criando card de task.

---
+++ src/components/Task/Task.tsx
@@ -0,0 +1,21 @@
+import { Check, Trash } from 'phosphor-react'
+import styles from './Task.module.css'
+
+export function Task(){ 
+    return(
+        <div className={styles.wrapper}>
+            <div className={styles.container}>
+                {/* <div className={styles.unchecked}>
+                .
+                </div> */}
+                <div className={styles.checked}>
+                    <Check/>
+                </div>
+                <p>Integer urna interdum massa libero auctor neque turpis turpis semper.</p>
+            </div>
+            <div  className={styles.trash}>
+                <Trash size={20}/>
+            </div>
+        </div>
+    )
+}
