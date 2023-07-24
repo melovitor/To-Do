8af52939cfeef0ebf154ff13eb5d136812ad22a9
melovitor <vitormelobatista@hotmail.com>
2023-07-24 12:01:21 -0300

    ENd - ajuste no localstorage.

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ interface TasksProps {
 }
 
 export default function App() {
-  localStorage.removeItem("todolistcounter");
   const storedTasks:any = localStorage.getItem("todolist");
   const storedTasksCounter:any = localStorage.getItem("todolistcounter");
   
